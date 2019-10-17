4ee62026e13a30972858ae1c1dc33ebdaa31a695
Nyiko-Ngwenya <nyikongwenya@gmail.com>
2019-10-17 10:10:04 +0200

    Done with classes and functionality

--- dice.py
+++ dice.py
@@ -1,24 +1,24 @@
 import random
 
 class Dice:
-    #random.seed(9001)
+    random.seed(9001)
     def __init__(self,sides,probabilities=[]):
         self.sides = sides
         self.value = 0
         self.probabilities = probabilities
-        if len(self.probabilities) > 0 and len(self.probabilities) != self.sides:
-            raise Exception
+        self.checkNegatives
+        self.checkSum
+        self.checkType
+        
        
     def roll(self):
         if len(self.probabilities) == 0:
-            self.value = random.randint(1,self.sides)
+            self.value = random.randint(1,self.sides+1)
         else:
             self.checkNegatives()
-            numbers = [1,2,3,4,5,6]
+            numbers = [ i for i in range(1,self.sides+1)]
             numbers_after = []
             for i in range(self.sides):
-                # # print(numbers[i],'The actaul number ')
-                # # print(self.probabilities[i], 'The probability')
                 if self.probabilities[i] >= 1:
                     for a in range(self.probabilities[i]):
                         numbers_after.append(numbers[i])
@@ -56,21 +56,23 @@ class Dice:
     def setProbabilities(self,new_probabilities):
         self.probabilities = new_probabilities
                 
+    def checkEqualSides(self):
+        if len(self.probabilities) > 0 and len(self.probabilities) != self.sides:
+            raise Exception
 
 dice_instance = Dice(6,[1,2,1,4,5,6])
-print(dice_instance.showProb())
+#print(dice_instance.showProb())
 dice_instance.roll()
 print(dice_instance.get_value())
-# dice_instance = Dice(20)
-# print(dice_instance.showProb())
-# dice_instance.roll()
-# print(dice_instance.get_value())
+
 class DiceFactory:
-    def __init__(self):
-        pass
+    def __init__(self,num_of_sides):
+        self.num_of_sides = num_of_sides
     def make_die(self):
-        pass
+        dice_instance = Dice(self.num_of_sides)
+        return dice_instance
+        
 
 factory20 = DiceFactory(20)
-die20 = factory20.make_ie()
+die20 = factory20.make_die()
 anotherDie20 = factory20.make_die()
